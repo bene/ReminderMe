bc9c0257c6851cf4a777ce193ebeafbba601dfaf
bene <allerberger.bene@gmail.com>
2020-09-29 20:14:13 +0200

    Initial Commit

---
+++ ReminderMe/ReminderMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderMeApp.swift
+//  ReminderMe
+//
+//  Created by Benedict on 29.09.20.
+//
+
+import SwiftUI
+
+@main
+struct ReminderMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
